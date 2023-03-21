5f30f98a961636358da1bdbf7f9392c64e584116
EmanHegy <emanhegy720@gamil.com>
2023-03-21 16:07:25 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Description: print "_putchar" word follwed by new line
+ *
+ * Retrurn: 0 (success)
+ */
+
+int main(void)
+{
+	printf ("_putchar\n");
+	return (0);
+}
