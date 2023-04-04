c89b81bd92cb34c11587224c0eb97925d186091f
EmanHegy <emanhegy720@gamil.com>
2023-04-03 17:15:51 -0700

    4-9

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@
  * Return: integer number
  */
 
-char _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - function locates the first occurrence of char in string
+ * of any of bytes in accept
+ *
+ * @s: input string 1
+ * @accept: input string 2
+ *
+ * Return: pointer to the matched byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char c;
+	int i, j;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				c = &s[i];
+				return (c);
+			}
+		}
+	}
+
+	return (0);
+}
